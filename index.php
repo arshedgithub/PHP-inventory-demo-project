1d37a0a533724c36ed3185f73156d5bdabfdc3f5
arshedgithub <arshedahmedmail@gmail.com>
2024-05-11 11:10:16 +0530

    retrieve data from database

--- index.php
+++ index.php
@@ -9,7 +9,18 @@
 
     echo "Database connection established...";
 
-    
+    // retrieve data from MySQL database
+
+    $sql = "SELECT * FROM product";
+    $result = mysqli_query($connection, $sql);
+
+    echo "<table border=1 cellpadding=5><tr><th>id</th><th>name</th><th>quantity</th><th>price</th></tr>";
+
+    while ($row = mysqli_fetch_assoc($result)) {
+       echo "<tr><td>". $row['id']. "</td><td>" .$row['name']. "</td><td>" .$row['quantity']. "</td><td>" .$row['price']. "</td></tr>";
+    }
+
+    echo "</table>";
 
 ?>
 
