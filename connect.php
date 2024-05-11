815b4acb0dd40550690b5d4901e1b8abbf4d6f16
arshedgithub <arshedahmedmail@gmail.com>
2024-05-11 12:05:55 +0530

    seperate connection file

---
+++ connect.php
@@ -0,0 +1,12 @@
+<?php
+
+    // connect to MySQL database
+    $connection = mysqli_connect("localhost", "root", "1234", "presem_demo");
+    
+    if (!$connection) {
+        die("Error : " . mysqli_connect_error());
+    }
+
+    echo "Database connection established...";
+
+?>
--- index.php
+++ index.php
@@ -1,16 +1,8 @@
 <?php
 
-    // connect to MySQL database
-    $connection = mysqli_connect("localhost", "root", "1234", "presem_demo");
-    
-    if (!$connection) {
-        die("Error : " . mysqli_connect_error());
-    }
-
-    echo "Database connection established...";
-
+    include "./connect.php";
+   
     // retrieve data from MySQL database
-
     $sql = "SELECT * FROM product";
     $result = mysqli_query($connection, $sql);
 
@@ -23,7 +15,7 @@
         <th>quantity</th>
         <th>price</th>
     </tr>
-    
+
     <?php while ($row = mysqli_fetch_assoc($result)) { ?>
         <tr>
             <td><?php echo $row['id'] ?></td>
