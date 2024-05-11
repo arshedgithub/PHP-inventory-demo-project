b988224f14ae046467fcf71f5558f990b8c6eeed
arshedgithub <arshedahmedmail@gmail.com>
2024-05-11 11:53:22 +0530

    embed php

--- index.php
+++ index.php
@@ -14,14 +14,23 @@
     $sql = "SELECT * FROM product";
     $result = mysqli_query($connection, $sql);
 
-    echo "<table border=1 cellpadding=5><tr><th>id</th><th>name</th><th>quantity</th><th>price</th></tr>";
-
-    while ($row = mysqli_fetch_assoc($result)) {
-       echo "<tr><td>". $row['id']. "</td><td>" .$row['name']. "</td><td>" .$row['quantity']. "</td><td>" .$row['price']. "</td></tr>";
-    }
-
-    echo "</table>";
-
 ?>
 
+<table border=1 cellpadding=5>
+    <tr>
+        <th>id</th>
+        <th>name</th>
+        <th>quantity</th>
+        <th>price</th>
+    </tr>
     
+    <?php while ($row = mysqli_fetch_assoc($result)) { ?>
+        <tr>
+            <td><?php echo $row['id'] ?></td>
+            <td><?php echo $row['name'] ?></td>
+            <td><?php echo $row['quantity'] ?></td>
+            <td><?php echo $row['price'] ?></td>
+        </tr>
+    <?php } ?>
+    
+</table> 
