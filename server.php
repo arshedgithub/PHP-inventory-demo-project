10b52e526b5e9b8c7d8977d413d695137d1c381b
arshedgithub <arshedahmedmail@gmail.com>
2024-05-18 11:27:18 +0530

    redirect page after adding data

--- server.php
+++ server.php
@@ -8,12 +8,13 @@
 
     if ($pro_name == "" || $quantity == "" || $price == "") {
         echo "Please fill all the fields...";
+        return;
     }
 
     $sql = "Insert into product (name, quantity, price) values ('$pro_name', '$quantity', '$price')";
     
     if (mysqli_query($connection, $sql)) {
-        echo "New record created successfully";
+        header("Location:./index.php");
     } else {
         echo "Error: ". mysqli_error($connection);
     }
